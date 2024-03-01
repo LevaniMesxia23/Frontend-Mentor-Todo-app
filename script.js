5864ce41754fb41598ceeb31ce1795fc3dd2427a
levan mesxia <levanmesxia@levans-MacBook-Pro.local>
2024-03-01 19:38:09 +0400

    updated button

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
 const input = document.querySelector("input")
-const button = document.querySelector("button")
 let span;
 let x;
 const container = document.querySelector(".container")
 const moon = document.querySelector(".moon")
 const sun = document.querySelector(".sun")
 const body = document.querySelector("body")
-
 const instBox = document.querySelector(".instruction-box")
+const newDiv = document.querySelector(".newDiv")
 
 input.addEventListener("keyup", (e) => {
   if (e.key === "Enter" && input.value !== "") {
     createDiv(input.value)
   }
 })
-let newDiv;
+
 function createDiv(inputValue) {
-  newDiv = document.createElement("div")
   newDiv.innerHTML += `<div class="added-div">
     <button><img src="./images/icon-check.svg" alt=""></button>
     <span class="main-span">${inputValue}</span>
@@ -24,35 +22,64 @@ function createDiv(inputValue) {
       <path fill-rule="evenodd" clip-rule="evenodd" d="M17.6777 0.707107L16.9706 0L8.83883 8.13173L0.707107 0L0 0.707107L8.13173 8.83883L0 16.9706L0.707106 17.6777L8.83883 9.54594L16.9706 17.6777L17.6777 16.9706L9.54594 8.83883L17.6777 0.707107Z" fill="#494C6B"/>
     </svg>
   </div>`
-  container.appendChild(newDiv)
-
-  x = newDiv.querySelector(".x")
-  x.addEventListener("click", () => {
-    input.value = ""
-    container.removeChild(newDiv);
+  
+  const addedDiv = document.querySelectorAll(".added-div")
+  addedDiv.forEach(addedDiv => {
+    const button = addedDiv.querySelector("button")
+    button.addEventListener("click", () => {
+      const span = addedDiv.querySelector("span")
+      if(span.classList.contains("main-span")){
+        span.classList.remove("main-span")
+        span.classList.add("spanCrossed")
+      }else if(span.classList = "spanCrossed"){
+        span.classList.remove("spanCrossed")
+        span.classList.add("main-span")
+      }
+      // span.classList.toggle("spanCrossed")
+    })
   })
+
+
+  // x = newDiv.querySelector(".x")
+  // let i = 1
+  // x.addEventListener("click", () => {
+  //   for(i; i < addedDiv.length; i++){
+  //     addedDiv.splice(i,1)
+  //     console.log(addedDiv.length)
+  //   }
+  // })
+  
+// const addedDiv = document.querySelector(".added-div") აქ გამოვიტანე
+// const button = newDiv.querySelector("button")
+//   button.addEventListener("click", () => {
+//     const span = newDiv.querySelector("span")
+//     if(span.classList.contains("main-span")){
+//       span.classList.remove("main-span")
+//       span.classList.add("spanCrossed")
+//     }else if(span.classList = "spanCrossed"){
+//       span.classList.remove("spanCrossed")
+//       span.classList.add("main-span")
+//     }
+//     // span.classList.toggle("spanCrossed")
+//   })
   input.value = ""
 }
-// const addedDiv = document.querySelector(".added-div") აქ გამოვიტანე
 
-  // button.addEventListener("click", () => {
-  //   span.classList.remove("main-span")
-  //   span.classList.add("spanCrossed")
-  // })
 const itemsLeft = document.querySelector(".items-left")
 const all = document.querySelector(".all")
 const active = document.querySelector(".active")
 const completed = document.querySelector(".completed")
 const clearCompleted = document.querySelector(".clear-completed")  
-
+let moonImage =  "./images/bg-desktop-dark.jpg"
+let sunImage =  "./images/bg-desktop-light.jpg"
   moon.addEventListener("click", () => {
     if(moon.style.display = "block"){
-      moon.style.display = "none"
-      sun.style.display = "block"
+      moon.style.display = "none";
+      sun.style.display = "block";
       body.style.backgroundColor = "#171823";
       input.style.backgroundColor = "#25273D";
       input.style.color = "#C8CBE7";
-
+      document.body.style.backgroundImage = 'url(' + moonImage + ')';
       itemsLeft.style.color = "#C8CBE7";
       all.style.color = "#C8CBE7";
       active.style.color = "#C8CBE7";
@@ -70,9 +97,11 @@ const clearCompleted = document.querySelector(".clear-completed")
   })
   sun.addEventListener("click", () => {
     if(sun.style.display = "block"){
-      sun.style.display = "none"
-      moon.style.display = "block"
+      sun.style.display = "none";
+      moon.style.display = "block";
       body.style.backgroundColor = "white";
+      document.body.style.backgroundImage = 'url(' + sunImage + ')';
+      
       input.style.backgroundColor = "#fff";
       
       itemsLeft.style.color = "#9495A5;";
